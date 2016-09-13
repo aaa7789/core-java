3adb05757757316b6cf3179bc85fa90834c8bb2b
Raji Komatreddy <rkomatreddy@webmd.net>
2016-09-13 14:27:08 -0400

    added more comments

--- JavaBasics/src/com/mms/variables/FooTest.java
+++ JavaBasics/src/com/mms/variables/FooTest.java
@@ -1,5 +1,11 @@
 package com.mms.variables;
 
+/**
+ * 
+ * @author RKomatreddy
+ *  method variables are stored in stack memory 
+ *  Objects and class level variables are stored in Heap (metaspace from java1.8) memory
+ */
 public class FooTest {
 	   public static void main(String[] args){
 	          Foo f = new Foo("f");
